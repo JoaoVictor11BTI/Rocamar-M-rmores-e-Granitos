a2ec52c9bd21c94ca6047d31e5874efff95e6015
João <joaovictor.tsouzaz@gmail.com>
2025-09-12 17:13:01 -0300

    Ah, pelo menos a responsividade do home eu tenho, essa bomba, falta fazer a responsividade do header no global (e de todo resto ;_;)

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ window.addEventListener("load", () => {
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
+if (hamburger && navMenu) {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active"); // animação do X
+    navMenu.classList.toggle("active");   // abrir/fechar menu
+  });
+}
